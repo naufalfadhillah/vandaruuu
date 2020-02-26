e5dc6f08b6b0d34e46167c28dfb92c7240d2de91
naufalfadhillah <naufalfadhillah80@gmail.com>
2020-02-27 00:09:57 +0700

    add new

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -8,7 +8,53 @@
         <div class="item" style="text-align : center">
             <!-- Place your content here to have it be centered vertically and horizontally  -->
             <h1>COMING SOON</h1>
-            <p>This dashboard page is under construction.</p>
+            <!-- <p>This dashboard page is under construction.</p> -->
+            <div class="row">
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                    <h3>150</h3>
+
+                    <p>New Orders</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                    <p>Bounce Rate</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                    <h3>44</h3>
+
+                    <p>User Registrations</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div>
+            </div>
         </div>
         </div>
     </div>
